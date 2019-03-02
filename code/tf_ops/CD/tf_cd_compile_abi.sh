a9336b7e78315cb0633f0693a5d81346627cf59d
Zhe Fu <fuzhe@yahoo.com>
2019-03-02 11:55:26 -0800

    changed to tf 1.3 from 1.12

--- code/tf_ops/CD/tf_cd_compile_abi.sh
+++ code/tf_ops/CD/tf_cd_compile_abi.sh
@@ -8,7 +8,7 @@ TF_LIB=$(python -c 'import tensorflow as tf; print(tf.sysconfig.get_lib())')
 
 
 $nvcc tf_nndistance_g.cu -c -o tf_nndistance_g.cu.o -std=c++11  -I $TF_INC -DGOOGLE_CUDA=1\
- -x cu -Xcompiler -fPIC -O2
+ -x cu -Xcompiler -fPIC -O2 -D_GLIBCXX_USE_CXX11_ABI=0 
 
 g++ tf_nndistance.cpp tf_nndistance_g.cu.o -o tf_nndistance_so.so -std=c++11 -shared -fPIC -I $TF_INC \
--I$TF_INC/external/nsync/public -L$TF_LIB -ltensorflow_framework -lcudart -L $cudalib
+-I$TF_INC/external/nsync/public -L$TF_LIB -ltensorflow_framework -lcudart -L $cudalib -D_GLIBCXX_USE_CXX11_ABI=0 
--- code/tf_ops/emd/tf_auctionmatch_compile_abi.sh
+++ code/tf_ops/emd/tf_auctionmatch_compile_abi.sh
@@ -4,8 +4,8 @@ cudalib=/usr/local/cuda-8.0/lib64/
 TF_INC=$(python -c 'import tensorflow as tf; print(tf.sysconfig.get_include())')
 TF_LIB=$(python -c 'import tensorflow as tf; print(tf.sysconfig.get_lib())')
 
-$nvcc tf_auctionmatch_g.cu  -c -o tf_auctionmatch_g.cu.o -std=c++11  -I $TF_INC -DGOOGLE_CUDA=1\
+$nvcc tf_auctionmatch_g.cu  -c -o tf_auctionmatch_g.cu.o -D_GLIBCXX_USE_CXX11_ABI=0  -std=c++11  -I $TF_INC -DGOOGLE_CUDA=1\
  -x cu -Xcompiler -fPIC -O2 -arch=sm_30
 
 g++ tf_auctionmatch.cpp tf_auctionmatch_g.cu.o -o tf_auctionmatch_so.so -std=c++11 -shared -fPIC -I $TF_INC \
--I$TF_INC/external/nsync/public -L$TF_LIB -ltensorflow_framework -lcudart -L $cudalib
+-I$TF_INC/external/nsync/public -L$TF_LIB -ltensorflow_framework -lcudart -L $cudalib -D_GLIBCXX_USE_CXX11_ABI=0  
