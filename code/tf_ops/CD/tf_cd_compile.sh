9d6e04d9c17cf4d8c77c478c2bc0ef1a6cd36601
Zhe Fu <fuzhe@yahoo.com>
2019-03-01 20:20:57 -0800

    fixing compile**abi.sh scripts

--- code/tf_ops/CD/tf_cd_compile.sh
+++ code/tf_ops/CD/tf_cd_compile.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
-nvcc=/usr/local/cuda-9.0/bin/nvcc
-cudalib=/usr/local/cuda-9.0/lib64/
+nvcc=/usr/local/cuda-8.0/bin/nvcc
+cudalib=/usr/local/cuda-8.0/lib64/
 TF_INC=$(python3 -c 'import tensorflow as tf; print(tf.sysconfig.get_include())')
 TF_LIB=$(python3 -c 'import tensorflow as tf; print(tf.sysconfig.get_lib())')
 
